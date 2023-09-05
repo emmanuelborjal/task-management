19b0928b5e38c72f7ab48b1f01df0bfcf306ce1f
Emmanuel F. Borjal <concretespecializedsolutions@gmail.com>
2023-09-05 23:16:08 +0800

    TICKET-1210: Add tailwind styling to the form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,8 +17,32 @@
             @apply rounded-md px-2 py-1 text-center font-medium shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-200 text-slate-700 bg-red-300
         }
 
+        .btn-cancel {
+            @apply rounded-md px-2 py-1 text-center font-medium shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-200 text-slate-700 bg-orange-300
+        }
+
+        .btn {
+            @apply rounded-md px-2 py-1 text-center font-medium shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-200 text-slate-700 bg-blue-300
+        }
+
         .link {
             @apply font-medium text-gray-700 underline decoration-pink-500
+        }
+
+        .error {
+            @apply text-red-500 text-sm
+        }
+
+        label {
+            @apply block uppercase text-slate-700 mb-2
+        }
+
+        input, textarea {
+            @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none
+        }
+
+        select {
+            @apply shadow-sm border w-1/3 py-2 px-3 text-slate-700 leading-tight focus:outline-none
         }
     </style>
     {{-- blade-formatter-enable --}}
